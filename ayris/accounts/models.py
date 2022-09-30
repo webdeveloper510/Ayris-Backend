e25f88631aa159e99bd19cb34fed730a07d9496a
Kajals22 <kajal@codenomad.net>
2022-09-30 11:47:28 +0530

    changes

--- ayris/accounts/models.py
+++ ayris/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import gettext_lazy as _
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django_countries.fields import CountryField
+# from django_countries.fields import CountryField
 
 from .manager import (
 CustomUserManager,

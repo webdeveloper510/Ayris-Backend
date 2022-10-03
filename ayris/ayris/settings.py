8f9a40119e990a7d6daf14ff3cac87fca02ac9b6
Kajals22 <kajal@codenomad.net>
2022-10-03 18:34:46 +0530

    changes

--- ayris/ayris/settings.py
+++ ayris/ayris/settings.py
@@ -33,7 +33,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
   'http://localhost:8080',
-  "http://54.167.85.36:8000/"
+  'http://54.167.85.36:8000/',
 )
 
 CORS_ALLOWED_ORIGINS = [
@@ -41,14 +41,16 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://54.167.85.36:8000/"
+  'http://54.167.85.36:8000/',
+
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://54.167.85.36:8000/"
+  'http://54.167.85.36:8000/',
+
 ]
 
 # Application definition
